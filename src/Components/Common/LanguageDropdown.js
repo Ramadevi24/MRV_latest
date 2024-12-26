3793002d6541db84346fd8d26cae4a024d92c12f
affan-mujawar-19 <affan.mujawar@ispatialtec.com>
2024-12-26 11:20:52 +0530

    Ui direction changes for Arabic and English

--- src/Components/Common/LanguageDropdown.js
+++ src/Components/Common/LanguageDropdown.js
@@ -1,37 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 import { get, map } from "lodash";
+import '../../assets/scss/CSS/styles.css'
 
-//i18n
+// i18n
 import i18n from "../../i18n";
 import languages from "../../common/languages";
 
-
 const LanguageDropdown = () => {
-    // Declare a new state variable, which we'll call "menu"
     const [selectedLang, setSelectedLang] = useState("");
+    const [isLanguageDropdown, setIsLanguageDropdown] = useState(false);
 
     useEffect(() => {
         const currentLanguage = localStorage.getItem("I18N_LANGUAGE");
         setSelectedLang(currentLanguage);
+
+        // Update body class
+        updateBodyClass(currentLanguage);
     }, []);
 
     const changeLanguageAction = lang => {
-        //set language as i18n
+        // Set language as i18n
         i18n.changeLanguage(lang);
         localStorage.setItem("I18N_LANGUAGE", lang);
         setSelectedLang(lang);
+
+        // Update body class
+        updateBodyClass(lang);
     };
 
-
-    const [isLanguageDropdown, setIsLanguageDropdown] = useState(false);
     const toggleLanguageDropdown = () => {
         setIsLanguageDropdown(!isLanguageDropdown);
     };
+
+    const updateBodyClass = (lang) => {
+        // Remove previous language class
+        document.body.classList.remove("lang-ar", "lang-en");
+
+        // Add the new language class
+        if (lang === "ar") {
+            document.body.classList.add("lang-ar");
+        } else {
+            document.body.classList.add("lang-en");
+        }
+    };
+
     return (
         <React.Fragment>
-            <Dropdown isOpen={isLanguageDropdown} toggle={toggleLanguageDropdown} className="ms-1 topbar-head-dropdown header-item">
-                <DropdownToggle className="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle shadow-none" tag="button">
+            <Dropdown 
+                isOpen={isLanguageDropdown} 
+                toggle={toggleLanguageDropdown} 
+                className="ms-1 topbar-head-dropdown header-item"
+            >
+                <DropdownToggle 
+                    className="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle shadow-none" 
+                    tag="button"
+                >
                     <img
                         src={get(languages, `${selectedLang}.flag`)}
                         alt="Header Language"
@@ -44,8 +68,7 @@ const LanguageDropdown = () => {
                         <DropdownItem
                             key={key}
                             onClick={() => changeLanguageAction(key)}
-                            className={`notify-item ${selectedLang === key ? "active" : "none"
-                                }`}
+                            className={`notify-item ${selectedLang === key ? "active" : ""}`}
                         >
                             <img
                                 src={get(languages, `${key}.flag`)}
